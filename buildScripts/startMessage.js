9e66806b3092426207141bf8ac7cf57f7304d890
Lakshmi S Chowdam <chowdam.net@gmail.com>
2017-01-06 20:06:22 -0500

    Babel and webpack configured

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
-var chalk = require('chalk');
-
-console.log(chalk.green("Starting dev environment now.."));
+//var chalk = require('chalk');
+import chalk from 'chalk';
+console.log(chalk.green("Starting dev environment now with babel.."));
